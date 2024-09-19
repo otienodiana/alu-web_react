6bd0cd77ecba0b338aa9acaf5a346c47d1dddbc4
Your Name <you@example.com>
2024-09-19 19:46:10 +0300

    corrected the code

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
@@ -10,9 +11,30 @@ import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeydown = this.handleKeydown.bind(this);
+  }
+
+  // Lifecycle Methods
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  // Handle Log out
+  handleKeydown(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
   render() {
-    // instantiate vars from props
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, logOut } = this.props;
 
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
@@ -24,27 +46,29 @@ class App extends React.Component {
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New course available' },
       { id: 3, type: 'urgent', html: htmlObj },
-    ];
+    ]
 
     return (
       <>
-        <Notifications displayDrawer={false} listNotifications={listNotifications} />
+        <Notifications displayDrawer={ false } listNotifications={ listNotifications } />
         <div className="App">
           <Header />
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          { isLoggedIn ? <CourseList listCourses={ listCourses } /> : <Login /> }
           <Footer />
         </div>
       </>
-    );
+    )
   }
 }
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
+  logOut: () => {},
 };
 
-export default App;
+export default hot(module)(App);
--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
@@ -48,4 +52,23 @@ describe('<App />', () => {
         const wrapper = shallow(<App isLoggedIn={ true } />);
         expect(wrapper.find(CourseList)).toHaveLength(1);
     });
+
+    it('verifies that the user canlog out using ctrl + h', () => {
+        const events = {};
+        window.addEventListener = jest.fn().mockImplementation((e, cb) => {
+            events[e] = cb;
+        });
+
+        const props = {
+            isLoggedIn: true,
+            logOut: jest.fn()
+        }
+        window.alert = jest.fn();
+
+        const wrapper = shallow(<App {...props} />);
+        events.keydown({ ctrlKey: true, key: 'h' });
+        expect(window.alert).toHaveBeenCalledWith("Logging you out");
+        expect(props.logOut).toHaveBeenCalled();
+        window.alert.mockRestore();
+    })
 });
