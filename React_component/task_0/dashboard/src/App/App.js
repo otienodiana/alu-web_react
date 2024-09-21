a263ec8a1392b4a645b6194a1fb9b030b5a8f6ba
Your Name <you@example.com>
2024-09-21 21:34:07 +0300

    corected errors

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
@@ -9,42 +10,46 @@ import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-class App extends React.Component {
-  render() {
-    // instantiate vars from props
-    const { isLoggedIn } = this.props;
-
-    const listCourses = [
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 },
     ];
-    const htmlObj = getLatestNotification();
-    const listNotifications = [
+
+    this.htmlObj = getLatestNotification();
+    this.listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New course available' },
-      { id: 3, type: 'urgent', html: htmlObj },
+      { id: 3, type: 'urgent', html: this.htmlObj },
     ];
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
 
     return (
       <>
-        <Notifications displayDrawer={false} listNotifications={listNotifications} />
+        <Notifications displayDrawer={false} listNotifications={this.listNotifications} />
         <div className="App">
           <Header />
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          {isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
           <Footer />
         </div>
       </>
     );
   }
+
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+  };
 }
 
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-};
-
-export default App;
+export default hot(module)(App);
